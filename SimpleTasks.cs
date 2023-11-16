2e61aaeb5908b458db7ac5ba534ad13ec2e91638
Gabriel <132488470+gabrielmaf22@users.noreply.github.com>
2023-11-16 18:34:21 -0300

    adicionando arquivo com alterações
    ------

--- SimpleTasks.cs
+++ SimpleTasks.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Collections.Generic;
 
@@ -6,8 +7,9 @@ namespace Sample
 {
     public class SimpleTasks
     {
+
         // retorna character com maior frequencia
-        public static char MaxCountChar(string str)
+        public static char CharMaxCount(string str)
         {
             Dictionary<char, int> dict = new Dictionary<char, int>();
             // preenche dict com a frequencia de cada char 
@@ -22,16 +24,19 @@ namespace Sample
             return dict.Aggregate((a, b) => a.Value > b.Value ? a : b).Key;
         }
 
+
         // retorna vogal com maior frequncia
-        public static char MaxCountVewel(string str)
+        public static char VewelMaxCount(string str)
         {
-            List<char> listChar = new List<char> {'a', 'e', 'i', 'o', 'u'};
+            char[] listVowels = new char[] {'a', 'e', 'i', 'o', 'u',
+                                             'A', 'E', 'I', 'O', 'U' };
+                                             
             Dictionary<char, int> dict = new Dictionary<char, int>();
 
             // preenche dict com a frequencia de cada vogal da string
             foreach( char ch in str )
             {
-                if(listChar.Contains(ch))
+                if(listVowels.Contains(ch))
                 {
                     if(!dict.ContainsKey(ch))
                         dict.Add(ch, 1);
@@ -43,8 +48,37 @@ namespace Sample
             return dict.Aggregate((a, b) => a.Value > b.Value ? a : b).Key;
         }
 
+
+        // retorna substring com maior frequencia de consoantes
+        public static string SubConsoantMaxCount(string str)
+        {
+            char[] listVowels = new char[] {'a', 'e', 'i', 'o', 'u',
+                                             'A', 'E', 'I', 'O', 'U' };
+            
+            // countConsoant possui cada indice correspondendo a cada string de subString
+            string[] subStrings = str.Split(' ');
+            int[] countConsoant = new int[subStrings.Length];
+
+            // preenche countConsoant com a frequencia de consoantes de cada substring
+            for(int rep = 0; rep < subStrings.Length; rep++)
+            {
+                foreach( char ch in subStrings[rep] )
+                {
+                    if(!listVowels.Contains(ch) && !Char.IsDigit(ch))
+                        countConsoant[rep]++;
+                }
+            }
+
+            // pegando o indice da substring com maior frequencia de consoante
+            int index = Array.IndexOf(countConsoant, countConsoant.Max());
+            return subStrings[index];
+        }
+
     }
 }
+
+
+
 
 
 
